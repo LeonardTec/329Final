bb58028c0fdbb70889316f502cfbfd494402a89e
EdMora08 <135734332+EdMora08@users.noreply.github.com>
2023-06-07 21:47:02 -0700

    Update control.c

--- control.c
+++ control.c
@@ -7,7 +7,6 @@
 
 #include "password.h"
 #include "lcd.h"
-#include "dac.h"
 #include "control.h"
 #include "keypad.h"
 
@@ -22,6 +21,7 @@ void control_room_select(void) {
 	LCD_write_string(" RM1  RM2  RM3 ");
 }
 
+// Function that polls for a keypad entry
 int control_keypad(void) {
 	int button = -1;
 	button = keypad_decode();				// Read from keypad
@@ -36,17 +36,18 @@ void control_light2or3(void) {
 	// Clears Screen
 	LCD_clear();
 	LCD_home();
-	// ROOM SELECTION SCREEN
+	// Display Options
 	LCD_write_string("1:ON     2:OFF");
 	LCD_second_line();
 	LCD_write_string("0:RETURN");
 }
 
+// Function that displays commands for LIGHTING in ROOM 1
 void control_light1(void) {
 	// Clears Screen
 	LCD_clear();
 	LCD_home();
-	// ROOM SELECTION SCREEN
+	// Display Options
 	LCD_write_string("1:ON     2:OFF");
 	LCD_second_line();
 	LCD_write_string("0:RETURN 9:HOME");
@@ -57,37 +58,43 @@ void control_room1_display(void) {
 	// Clears Screen
 	LCD_clear();
 	LCD_home();
-	// ROOM SELECTION SCREEN
+	// Display Options
 	LCD_write_string("1:LIGHTS   2:FAN");
 	LCD_second_line();
 	LCD_write_string("9:HOME");
 }
 
+// Function that displays commands for Brighting/Dimming
 void control_brightness(void) {
 	// Clears Screen
 	LCD_clear();
 	LCD_home();
+	// Display Options
 	LCD_write_string("1:BRIGHT 2:DIM");
 	LCD_second_line();
 	LCD_write_string("0:RETURN 9:HOME");
 
 }
 
+// // Function that displays commands for FAN
 void control_fan(void) {
+	// Clears Screen
 	LCD_clear();
 	LCD_home();
+	// Display Options
 	LCD_write_string("1:ON/OFF");
 	LCD_second_line();
 	LCD_write_string("0:RETURN 9:HOME");
 }
 
+// Function that initializes FAN GPIO PIN
 void control_fan_init(void) {
 	// Enable Clocks
 	RCC->AHB2ENR |= MOTOR_PORT_EN;
 
 	// Set GPIO Mode to Output
 	MOTOR_PORT -> MODER   &= ~(GPIO_MODER_MODE0);
-	MOTOR_PORT -> MODER   |= GPIO_MODER_MODE1_0;
+	MOTOR_PORT -> MODER   |= GPIO_MODER_MODE0_0;
 
 	// Push-Pull Reset
 	MOTOR_PORT -> OTYPER  &= ~( GPIO_OTYPER_OT0 );
